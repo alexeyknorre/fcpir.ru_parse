f22b54e50c5fe2ef89297397494a9433cb79bfa3
Alexey Knorre <aknorre@eu.spb.ru>
2019-04-10 20:08:02 +0300

    fix master-script

--- code/master-script.R
+++ code/master-script.R
@@ -1,29 +1,20 @@
-# install pacman to streamline further package installation
-if (!require("pacman", character.only = TRUE)){
-  install.packages("pacman", dep = TRUE)
-  if (!require("pacman", character.only = TRUE))
-    stop("Package not found")
+# check nedded libraries
+if (!require("tidyverse", character.only = TRUE)){
+  install.packages("tidyverse", dep = TRUE)
 }
 
-
-# these are the required packages
-pkgs <- c(
-  "tidyverse", 
-  "openxlsx",
-  "rvest")
-
-# install the missing packages
-# only run if at least one package is missing
-if(!sum(!p_isinstalled(pkgs))==0){
-  p_install(
-    package = pkgs[!p_isinstalled(pkgs)], 
-    character.only = TRUE
-  )
+if (!require("openxlsx", character.only = TRUE)){
+  install.packages("openxlsx", dep = TRUE)
 }
+
+if (!require("rvest", character.only = TRUE)){
+  install.packages("rvest", dep = TRUE)
+}
+
 
 # Create folders
-dir.create("data"), showWarnings = FALSE)
-dir.create("results"), showWarnings = FALSE)
+dir.create("data", showWarnings = FALSE)
+dir.create("results", showWarnings = FALSE)
 
 # Run scripts
 eval(parse("code/1_parse_links.R", encoding = "UTF-8"))
